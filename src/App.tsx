0a6035bd4b72936c64e3e8dbcb89d802db005bf9
hafiz ali <hafizdidarali@gmail.com>
2023-02-23 21:12:24 +0000

    created sections for board and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import styles from './App.module.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div>left</div>
-      <div>right</div>
-    </div>
+    <main className={styles.container}>
+      <section className={styles.left}>board section</section>
+      <section className={styles.right}>input and result section</section>
+    </main>
   )
 }
 
