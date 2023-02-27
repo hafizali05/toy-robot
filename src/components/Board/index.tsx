d92c889bef56d6e7ef68748c845d06e6127e6d64
hafiz ali <hafizdidarali@gmail.com>
2023-02-27 19:26:05 +0000

    showing robot on UI by co-ordinates

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,15 +1,36 @@
+import classNames from 'classnames';
+import { matrix } from '../../contants/matrix';
 import styles from './style.module.css';
 const styleClass = [styles.blue, styles.red];
-const getBackgroundColor = (index: number) => styleClass[index % 2];
+
+const rows = [0, 1, 2, 3, 4];
+const columns = [0, 1, 2, 3, 4];
+
+
 export const Board = () => {
     return (
-        <div className={styles.parent} data-testid="board">
+        <div className={classNames(styles.parent)} data-testid="board">
             {
-                Array.from({ length: 25 })
-                    .map(
-                        (e, index) => <div key={index} className={getBackgroundColor(index)} />
+                matrix.map(
+                    ([x, y], index) => (
+                        <div
+                            aria-label={`row ${x} and column ${y}`}
+                            key={index}
+                            className={classNames([styleClass[index % 2], styles.box])}
+                            data-x={x}
+                            data-y={y}
+                        >
+                            <Robot bol={x === 1 && y === 1} />
+                        </div>
                     )
+                )
             }
         </div>
+    )
+}
+const Robot = ({ bol }: { bol: boolean }) => {
+    if (!bol) return null;
+    return (
+        <img src="/public/robot.svg" alt="" style={{ position: 'absolute' }} width={100} />
     )
 }
